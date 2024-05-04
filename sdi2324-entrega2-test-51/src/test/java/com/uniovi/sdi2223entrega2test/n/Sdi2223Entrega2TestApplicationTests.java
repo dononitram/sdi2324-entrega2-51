aa0e17d237e37cb9a1cdd11e369a9c35efa0fc68
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2024-05-04 17:42:07 +0200

    Tests 18, 20 and 21 from [W8] completed

--- sdi2324-entrega2-test-51/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
+++ sdi2324-entrega2-test-51/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
@@ -403,6 +403,73 @@ class Sdi2223Entrega2TestApplicationTests {
     }
 
     /**
+     * @author Pedro
+    [Prueba19] Hacer una búsqueda con el campo vacío y comprobar que se muestra la página que
+    corresponde con el listado usuarios existentes en el sistema.
+     */
+    @Test
+    @Order(19)
+    void PR19() {
+        //Inicio sesión como el user1
+        PO_PublicView.loginSpecificUser("user01@email.com", "Us3r@1-PASSW", driver);
+        //Ir a la lista de usuarios de la red
+        PO_PrivateView.click(driver, "id", "mylistUsersSocial", 0);
+        //Busco sin criterio
+        PO_PrivateView.click(driver, "id", "search-button", 0);
+        // Compruebo que no se muestra el user1 (usuario actual) ni el admin
+        SeleniumUtils.textIsNotPresentOnPage(driver, "user01@email.com");
+        SeleniumUtils.textIsNotPresentOnPage(driver, "admin@email.com");
+        //Ir a la última página, en este caso la tercera
+        PO_Pagination.clickPage(driver, 3);
+        SeleniumUtils.textIsPresentOnPage(driver, "user15@email.com");
+    }
+
+    /**
+     * @author Pedro
+    [Prueba20] Hacer una búsqueda escribiendo en el campo un texto que no exista y comprobar que se
+    muestra la página que corresponde, con la lista de usuarios vacía.
+     */
+    @Test
+    @Order(20)
+    void PR20() {
+        //Inicio sesión como el user1
+        PO_PublicView.loginSpecificUser("user01@email.com", "Us3r@1-PASSW", driver);
+        //Ir a la lista de usuarios de la red
+        PO_PrivateView.click(driver, "id", "mylistUsersSocial", 0);
+        //Busco por zzz que no debe salir nada
+        PO_PrivateView.searchBy(driver, "zzz");
+        // Compruebo que no se muestra el user1 (usuario actual) ni el admin
+        SeleniumUtils.textIsNotPresentOnPage(driver, "user01@email.com");
+        SeleniumUtils.textIsNotPresentOnPage(driver, "admin@email.com");
+        // Tampoco sale el usuario 2
+        SeleniumUtils.textIsNotPresentOnPage(driver, "user02@email.com");
+        //Está vacía la tabla
+    }
+
+    /**
+     * @author Pedro
+    [Prueba21] Hacer una búsqueda con un texto específico y comprobar que se muestra la página que
+    corresponde, con la lista de usuarios en los que el texto especificado sea parte de su nombre,
+    apellidos o de su email.
+     */
+    @Test
+    @Order(21)
+    void PR21() {
+        //Inicio sesión como el user1
+        PO_PublicView.loginSpecificUser("user01@email.com", "Us3r@1-PASSW", driver);
+        //Ir a la lista de usuarios de la red
+        PO_PrivateView.click(driver, "id", "mylistUsersSocial", 0);
+        //Busco por 2 que debería salir el usuario 2 y 12
+        PO_PrivateView.searchBy(driver, "2");
+        // Compruebo que no se muestra el user1 (usuario actual) ni el admin
+        SeleniumUtils.textIsNotPresentOnPage(driver, "user01@email.com");
+        SeleniumUtils.textIsNotPresentOnPage(driver, "admin@email.com");
+        // Salen el usuario 2 y el 12
+        SeleniumUtils.textIsPresentOnPage(driver, "user02@email.com");
+        SeleniumUtils.textIsPresentOnPage(driver, "user12@email.com");
+    }
+
+    /**
      * @author Samuel
      * [Prueba22] Desde el listado de usuarios de la aplicación, enviar una invitación de amistad a un usuario.
      * Comprobar que la solicitud de amistad aparece en el listado de invitaciones (punto siguiente).
