7d3fb8854df731ba8a7571002cf918bd04e44080
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2024-05-04 20:15:29 +0200

    Fixed test 27 with the new inserted publications

--- sdi2324-entrega2-test-51/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
+++ sdi2324-entrega2-test-51/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
@@ -755,10 +755,10 @@ class Sdi2223Entrega2TestApplicationTests {
         SeleniumUtils.textIsPresentOnPage(driver, "user05@email.com");
         //Además sale la fecha de ambos y el texto de la últ publi del user 5 tanto como el de ninguna publi del 4
         //User 5
-        SeleniumUtils.textIsPresentOnPage(driver, "PublicationTest");
+        SeleniumUtils.textIsPresentOnPage(driver, "Publication 50");
         SeleniumUtils.textIsPresentOnPage(driver, "28 Apr 2024");
         //User 4
-        SeleniumUtils.textIsPresentOnPage(driver, "Nothing published");
+        SeleniumUtils.textIsPresentOnPage(driver, "Publication 40");
         SeleniumUtils.textIsPresentOnPage(driver, "29 Apr 2024");
     }
 
